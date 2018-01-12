2b55714b91f3a22901f30141585dc73a4ec738aa
Min Park <minpark0212@gmail.com>
2018-01-11 19:18:23 -0500

    initial

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'oiHiBJDpK3wIGRfHz9esQJTfl',
+  consumer_secret: 'JEJqTZSgkD9D5OppaVab2WwGxDPt5OZexGkkSmLjeerNpFdYyZ',
+  access_token_key: '951181261518856192-nOuMf5QejhLTvOmi4YR0PNL7OdtFa9n',
+  access_token_secret: 'wXIgC8h3V6tlPjejfmGWSESKq0MHeA64Ap316XjCgO9nf',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,6 @@
+
+var keys = require("./keys.js");
+
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
